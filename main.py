301495686a04df6e9aaafe0519a4e33199dc159a
Jdka1 <staryan.mehra@gmail.com>
2022-10-12 12:52:18 -0700

    Saving image as lower quality

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ def convert_to_jpg(infile):
         try:
             with Image.open(infile) as to_jpg:
                 rgb_im = to_jpg.convert('RGB')
-                rgb_im.save(outfile)
+                for i in range(1, 100, 5):
+                    x = rgb_im
+                    x.save(outfile, quality=i)
         except Exception as e:
             print(e)
 
