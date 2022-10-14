38c299a8d36ca75af8435dd59de33df46944be32
Jdka1 <staryan.mehra@gmail.com>
2022-10-14 12:41:41 -0700

    get hashtags

---
+++ gethashtags.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+
+[print('\n') for i in range(10)]
+
+def get_hashtags(topic, num):
+    url = f'http://best-hashtags.com/hashtag/{topic}/'
+    html = requests.get(url=url).text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    ptags = [soup.find(f'p{i}').text.split(' ') for i in range(1,4)]
+    hashtags = []
+    for ptag_list in ptags:
+        hashtags += ptag_list
+    
+    hashtags = [hashtag for hashtag in hashtags if hashtag]
+    
+    return [random.choice(hashtags) for i in range(min(num,len(hashtags)))]
+
+
+print(get_hashtags('landscapephotography', 20))
---
+++ to_jpg.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import os, sys
+import shutil
+import time
+
+def convert_to_jpg(infile, portrait):
+    try:
+        shutil.rmtree('Converted IMGs')
+    except Exception as e:
+        print(e)
+
+
+    os.mkdir('/Users/arymehr/Documents/CS Projects/Image Resizing/Converted IMGs')
+
+    removed_filetype = ''.join(infile.split('.')[:-1])
+    outfile = f"Converted IMGs/{removed_filetype}.jpg"
+    
+    if infile != outfile:
+        try:
+            with Image.open(infile) as to_jpg:
+                rgb_im = to_jpg.convert('RGB')
+                rgb_im.thumbnail((1080, 1080)) # limiting width and height to 1080p
+                rgb_im.save(outfile)
+
+        except Exception as e:
+            print(e)
+
+
+
+
+
+convert_to_jpg('testim.png', portrait=False)
