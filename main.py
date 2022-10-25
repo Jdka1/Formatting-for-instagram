25dc0187ce92db23ef32e781014812aabaf0a11a
Jdka1 <staryan.mehra@gmail.com>
2022-10-24 18:11:56 -0700

    creds

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from scripts.createpost import create_post
 from PIL import Image
+from credentials import creds
 
 
-topics = ["photography","snow"]
-numtags = 20
-imgpath = "/Users/aryanmehra/Downloads/4V7A3707.jpg"
+# topics = ["photography","snow"]
+# numtags = 20
+# imgpath = "/Users/aryanmehra/Downloads/4V7A3707.jpg"
 
-with Image.open(imgpath) as f:
-    create_post(
-        topics=topics,
-        numtags=numtags,
-        imgpath="/Users/aryanmehra/Downloads/4V7A3707.jpg"
-    )
+# with Image.open(imgpath) as f:
+#     create_post(
+#         topics=topics,
+#         numtags=numtags,
+#         imgpath="/Users/aryanmehra/Downloads/4V7A3707.jpg"
+#     )
